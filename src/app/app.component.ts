d9cdeb24115410d44d18edf3419be28f543e0366
BoweiWei <bowei_wei@brown.edu>
2019-11-13 17:04:22 -0500

    game finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Food } from '../../shared/food.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cooking-Game';
+  enternew = false;
+  food_counter = 0;
+  foods: Food[] = [];
+  new_food = new Food("")
+  receipt: Food[] = [new Food('meat'), new Food('apple'), new Food('tomato')].sort();
+  answer = true;
+  show_answer = false;
+
+  // Event Binding and record new food
+  onUpdateFood(event: Event) {
+    this.new_food = new Food((<HTMLInputElement>event.target).value);
+
+  }
+
+  // save the new food into food
+  submit() {
+    this.foods.push(this.new_food);
+    this.foods.sort();
+    this.enternew = true;
+    console.log(this.foods);
+    this.food_counter ++;
+    this.check();
+  }
+
+  reset() {
+    this.foods = [];
+    this.enternew = false;
+    this.show_answer = false;
+    this.answer = true;
+  }
+
+  check() {
+    if (this.food_counter >= 3) {
+      this.show_answer = true;
+      for (var i = 0; i < this.foods.length; i++) {
+        if (this.foods[i] != this.receipt[i]) {
+          this.answer = false;
+        }
+      }
+    }
+  }
 }
